af827617c0eef19e45ae37adfaaf83b5b722779d
Bernardo Ramos <up201505092@fe.up.pt>
2020-06-14 16:26:39 +0100

    Updates

---
+++ precache-manifest.aa5da0d4491817251b2b3a300e771447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f3e4b75b126c81bd840f965911a549",
+    "url": "/beerzyp.github.io/index.html"
+  },
+  {
+    "revision": "acbfb77b7fe47fa92b81",
+    "url": "/beerzyp.github.io/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "32554d0cb67cea3067de",
+    "url": "/beerzyp.github.io/static/js/2.811a1c0e.chunk.js"
+  },
+  {
+    "revision": "acbfb77b7fe47fa92b81",
+    "url": "/beerzyp.github.io/static/js/main.8ad0e67f.chunk.js"
+  },
+  {
+    "revision": "bcdc4723881359e71de5",
+    "url": "/beerzyp.github.io/static/js/runtime~main.6575fc0f.js"
+  }
+]);
